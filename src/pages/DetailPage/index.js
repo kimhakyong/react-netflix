c91fe7d09b4489650d13a06e8005fc7b23339189
kimhakyong <kimhakyong@gmail.com>
2024-01-18 17:44:13 +0900

    init

---
+++ src/pages/DetailPage/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "../../api/axios";
+
+function DetailPage(props) {
+  const [movie, setMovie] = useState({});
+
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    fetchData();
+  }, [movieId]);
+
+  async function fetchData() {
+    const request = await axios.get(`/movie/${movieId}`);
+    setMovie(request.data);
+  }
+
+  if (!movie) return <div>...loading</div>;
+
+  return (
+    <section>
+      <img
+        className="modal__poster-img"
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        alt="poster"
+      />
+    </section>
+  );
+}
+
+export default DetailPage;
---
+++ src/pages/SearchPage/index.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
+import "./SearchPage.css";
+import { useDebounce } from "../../hooks/useDebounce";
+
+function SearchPage(props) {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const navigate = useNavigate();
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  useEffect(() => {
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
+    }
+  }, [debouncedSearchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(`/search/multi?query=${searchTerm}`);
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+
+            return (
+              <div className="movie" key={movie.id}>
+                <div
+                  onClick={() => navigate(`/${movie.id}`)}
+                  className="movie__column-poster"
+                >
+                  <img
+                    src={movieImageUrl}
+                    alt="movie image"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾고자하는 검색어 "{debouncedSearchTerm}"</p>에 맞는 영화가
+          없습니다.
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
+}
+
+export default SearchPage;
